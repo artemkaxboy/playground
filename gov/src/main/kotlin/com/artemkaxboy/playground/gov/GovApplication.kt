be79e48c47cbc1aaa17a80635c155cdc9c177c4f
Artem Kolin <artemkaxboy@gmail.com>
2022-11-06 12:59:40 +0400

    Adds new gradle/kotlin/spring/flyway module `gov`

---
+++ gov/src/main/kotlin/com/artemkaxboy/playground/gov/GovApplication.kt
@@ -0,0 +1,11 @@
+package com.artemkaxboy.playground.gov
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GovApplication
+
+fun main(args: Array<String>) {
+    runApplication<GovApplication>(*args)
+}
---
+++ gov/src/test/kotlin/com/artemkaxboy/playground/gov/GovApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.artemkaxboy.playground.gov
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GovApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
