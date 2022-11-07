87edff1b07369690106e387360ec0d971bedc4e0
Artem Kolin <artemkaxboy@gmail.com>
2022-11-07 23:52:17 +0400

    Commission.kt

--- gov/src/main/kotlin/com/artemkaxboy/playground/gov/dumagovru/entity/Commission.kt
+++ gov/src/main/kotlin/com/artemkaxboy/playground/gov/dumagovru/entity/Commission.kt
@@ -11,17 +11,23 @@ data class Commission(
 
     @Id
     val id: Long,
+
     @Column(columnDefinition = "TEXT")
     val title: String,
+
     @Column(columnDefinition = "TEXT")
     val description: String,
+
     @Column(columnDefinition = "TEXT")
     val type: String,
+
     @Column(name = "url_website", columnDefinition = "TEXT")
     val urlWebsite: String,
-    @Column(name = "short_title", nullable = true, columnDefinition = "TEXT")
+
+    @Column(name = "short_title", columnDefinition = "TEXT")
     val shortTitle: String?,
 ) {
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
