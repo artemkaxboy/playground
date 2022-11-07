910524e69c8036821eae4dc2fe387ef6c1af4822
Artem Kolin <artemkaxboy@gmail.com>
2022-11-07 23:53:52 +0400

    Commission

--- gov/src/main/kotlin/com/artemkaxboy/playground/gov/dumagovru/dto/CommissionDto.kt
+++ gov/src/main/kotlin/com/artemkaxboy/playground/gov/dumagovru/dto/CommissionDto.kt
@@ -7,11 +7,16 @@ import kotlinx.serialization.Serializable
 data class CommissionDto(
 
     val id: Long,
+
     val title: String,
+
     val description: String,
+
     val type: String,
+
     @SerialName("url_website")
     val urlWebsite: String,
+
     @SerialName("short_title")
     val shortTitle: String?,
 )
